2967bada098309a8a2992a2b7f8cf711d6ad8847
Ismail <63372291+elixit@users.noreply.github.com>
2023-10-12 21:03:08 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
-import 'mdbreact/dist/css/mdb.css';  // Adjust the import statement for MDB React UI Kit
-import App from './App';
+import React from 'react'; // Import React library
+import ReactDOM from 'react-dom'; // Import ReactDOM library
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter for routing
+import App from './App'; // Import the main App component
 
+// Create a root element to render the app
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the app wrapped in React.StrictMode
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
-      <App />
+    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}> 
+      {/* Set up routing with a base URL */}
+      <App /> {/* Render the main App component */}
     </BrowserRouter>
   </React.StrictMode>
 );
