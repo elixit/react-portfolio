3dd61973cde4e3b8fa33f97353b2b981ca244413
Ismail <63372291+elixit@users.noreply.github.com>
2023-10-12 20:57:39 -0700

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,15 +3,14 @@ import React from 'react';
 const About = () => {
   return (
     <div className="about-container">
-      {/* Start of the About section */}
+      {/* About section */}
       <div className="content-container">
         {/* Container for the content */}
         <div className="profile-info">
           {/* Profile Information */}
           <h5>Ismail Khan</h5>
-          {/* Display the name */}
           <p>
-            I'm embarking on a journey in web development, constantly expanding my skills to create a diverse portfolio.
+            I'm starting out in web development, constantly expanding my skills to create a diverse portfolio.
           </p>
           {/* Display a brief description */}
         </div>
